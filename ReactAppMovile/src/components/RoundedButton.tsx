5de373c5ee2d0bed56bc8adf3b5890532fe692a6
menita2005 <128631661+menita2005@users.noreply.github.com>
2024-03-19 18:13:46 -0500

    create new AppReactive

---
+++ ReactAppMovile/src/components/RoundedButton.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { MyColors } from '../theme/AppTheme';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native'
+interface Props {
+    text: string;
+    onPress: () => void,
+   }
+   export const RoundedButton = ({ text, onPress}: Props) => {
+    return (
+    <TouchableOpacity
+    style={styles.RoundedButton}
+    onPress={() => onPress()}
+    >
+    <Text style={styles.textButton} >{ text}</Text>
+    </TouchableOpacity>
+    )
+   }
+const styles = StyleSheet.create({
+ RoundedButton: {
+ width: '100%',
+ height: 50,
+ backgroundColor: MyColors.primary, 
+ alignItems: 'center',
+ justifyContent: 'center',
+ borderRadius: 15,
+ },
+ textButton: {
+ color: 'white',
+ }
+});
